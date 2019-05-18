bc7f400bb56c6c67bf86743e99d3bfffac62a7ec
Bo Zhang <bo@gradle.com>
2019-05-18 20:19:56 +0800

    More repositories

---
+++ src/test/java/com/github/hcsp/MainTest.java
@@ -0,0 +1,24 @@
+package com.github.hcsp;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import org.junit.jupiter.api.Test;
+
+public class MainTest {
+    @Test
+    public void importStringUtilsCorrectly() {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(baos, true));
+
+        Main.main(null);
+
+        assertTrue(new String(baos.toByteArray()).toLowerCase().contains("hello"));
+    }
+
+    @Test
+    public void hasGetMessageMethod() throws Exception {
+        Main.class.getDeclaredMethod("getMessage");
+    }
+}
