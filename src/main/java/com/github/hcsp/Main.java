27e99288189fada3b231731800cbec490471f04a
Hana <384073877@qq.com>
2019-07-03 22:38:42 +0800

    编写一个getMessage()方法，返回字符串"Hello"

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -1,11 +1,19 @@
 package com.github.hcsp;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println(getMessage());
+        String x = "Hello";
+        getMessage(x);
+        System.out.println(getMessage(x));
+        // Create a getMessage() method here to return string "Hello"
+        // 在这里编写一个getMessage()方法，返回字符串"Hello"
     }
+    public static String getMessage(String x){
+        return x ;
 
-    // Create a getMessage() method here to return string "Hello"
-    // 在这里编写一个getMessage()方法，返回字符串"Hello"
+
+    }
 
 }
