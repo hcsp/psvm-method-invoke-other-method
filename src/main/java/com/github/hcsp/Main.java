f6c9797d2bf322c292bde8c378f5d5bbca0d982b
zhouchangde <55309927+zhouchangde@users.noreply.github.com>
2019-09-17 22:43:50 +0600

    练习 (#85)
    练习 (#85)

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -4,7 +4,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println(getMessage());
     }
-
+static String getMessage(){
+        return "hello";
+    }
     // Create a getMessage() method here to return string "Hello"
     // 在这里编写一个getMessage()方法，返回字符串"Hello"
 
