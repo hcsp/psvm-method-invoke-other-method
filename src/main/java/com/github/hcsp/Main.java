0f80752ea4ecf200041aab1d71209786900a9205
SUPER Administrator <1276119527@qq.com>
2019-10-11 22:50:13 +0800

    编写成功

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -2,9 +2,16 @@ package com.github.hcsp;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(getMessage("Hello"));
+        System.out.println(getMessage());
     }
-    public static  String getMessage(String num){
+    public static  String getMessage(){
+        String num="Hello";
         return num;
+
+
     }
+
+    // Create a getMessage() method here to return string "Hello"
+    // 在这里编写一个getMessage()方法，返回字符串"Hello"
+
 }
