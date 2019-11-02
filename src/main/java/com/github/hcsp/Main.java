341b60af40c500f71e62a1f34f2a1c4f3c553b2b
liuxing <1151879967@qq.com>
2019-11-02 20:41:11 +0800

    已完成编写方法

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -1,8 +1,14 @@
 package com.github.hcsp;
 
 public class Main {
+    private static boolean hello;
+
     public static void main(String[] args) {
         System.out.println(getMessage());
+    }
+
+    private static boolean getMessage() {
+    return hello;
     }
 
     // Create a getMessage() method here to return string "Hello"
