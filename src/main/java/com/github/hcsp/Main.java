290bb57e2575b3d96beb9d09d51b0af4bfc6d890
nicknessii <545310317@qq.com>
2019-11-29 17:55:18 +0800

    编写一个方法

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -2,8 +2,11 @@ package com.github.hcsp;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("getMessage()");
+        System.out.println(getMessage());
     }
-   // Create a getMessage() method here to return string "Hello"
+    public static String  getMessage() {
+        return "Hello";
+    }
+    // Create a getMessage() method here to return string "Hello"
     // 在这里编写一个getMessage()方法，返回字符串"Hello"
 }
