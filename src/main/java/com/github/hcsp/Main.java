259db604718fa12003b87fdf2f03053b6ba66f3d
nicknessii <545310317@qq.com>
2019-11-29 13:48:05 +0800

    还原

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -2,11 +2,8 @@ package com.github.hcsp;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("wwww");
+        System.out.println("getMessage()");
     }
-
-
-    // Create a getMessage() method here to return string "Hello"
+   // Create a getMessage() method here to return string "Hello"
     // 在这里编写一个getMessage()方法，返回字符串"Hello"
-
 }
