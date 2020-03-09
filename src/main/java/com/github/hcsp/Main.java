3226edd13e87162938b2c01b118899552267700a
ShanQing Huang <58178504+7952126@users.noreply.github.com>
2020-03-09 15:24:12 +0800

    Abc (#183)
    Abc (#183)

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -2,7 +2,12 @@ package com.github.hcsp;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(getMessage());
+        getMessage();
+    }
+
+    public static void getMessage() {
+        System.out.println("hello");
+
     }
 
     // Create a getMessage() method here to return string "Hello"
