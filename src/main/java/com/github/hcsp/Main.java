8b341e4facc78690787a0e0193c5efc5e0305976
badpork <65276491+badpork@users.noreply.github.com>
2020-05-16 19:26:58 +0800

    Create a getMessage() method here to return string "Hello" (#211)
    Create a getMessage() method here to return string "Hello" (#211)

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -1,11 +1,14 @@
 package com.github.hcsp;
 
+import com.sun.org.apache.xpath.internal.objects.XString;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println(getMessage());
     }
-
     // Create a getMessage() method here to return string "Hello"
     // 在这里编写一个getMessage()方法，返回字符串"Hello"
-
+    public static String getMessage (){
+        return "Hello";
+    }
 }
