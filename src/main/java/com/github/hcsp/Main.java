765ac89aa17e1aaad6b4321d75c5a5eab770d040
haileeliang <haileeliang62@gmail.com>
2020-08-02 22:33:39 +0900

    编写一个方法，返回字符串"Hello"。

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -1,10 +1,15 @@
 package com.github.hcsp;
 
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+import org.w3c.dom.ls.LSOutput;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println(getMessage());
+        getMessage();
     }
-
+    public static void getMessage(){
+        System.out.println("Hello");
+    }
     // Create a getMessage() method here to return string "Hello"
     // 在这里编写一个getMessage()方法，返回字符串"Hello"
 
