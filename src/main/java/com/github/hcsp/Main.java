92e15eedc2e46f5ffd9f1be7d2e866356510858e
xu-kai-quan <50357378+xu-kai-quan@users.noreply.github.com>
2020-12-04 20:56:46 +0800

    编写一个getMessage()方法，返回字符串"Hello" (#247)
    编写一个getMessage()方法，返回字符串"Hello" (#247)

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -7,5 +7,7 @@ public class Main {
 
     // Create a getMessage() method here to return string "Hello"
     // 在这里编写一个getMessage()方法，返回字符串"Hello"
-
+    public static String getMessage(){
+        return  "Hello";
+    }
 }
