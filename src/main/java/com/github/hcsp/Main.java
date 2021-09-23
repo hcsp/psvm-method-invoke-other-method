d60d2750bc125af46d74f768c605c43019991a1a
YJL <2567843704@qq.com>
2021-09-23 16:06:07 +0800

    3

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -3,11 +3,11 @@ package com.github.hcsp;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println();
+        System.out.println(getMessage ());
     }
     // Create a getMessage() method here to return string "Hello"
     // 在这里编写一个getMessage()方法，返回字符串"Hello"
     public static String getMessage () {
-        return  "Hello";
+        return  "hello";
     }
 }
