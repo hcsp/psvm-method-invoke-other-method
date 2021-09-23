696964610f533e0dcb45fd2779e7e340defaa107
YJL <2567843704@qq.com>
2021-09-23 13:19:24 +0800

    2

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -1,7 +1,5 @@
 package com.github.hcsp;
 
-import com.sun.org.apache.xpath.internal.objects.XString;
-import sun.jvm.hotspot.HelloWorld;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,7 +7,7 @@ public class Main {
     }
     // Create a getMessage() method here to return string "Hello"
     // 在这里编写一个getMessage()方法，返回字符串"Hello"
-    public static String getmessage () {
+    public static String getMessage () {
         return  "Hello";
     }
 }
