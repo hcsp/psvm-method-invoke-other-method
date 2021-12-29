fd42a369106fc9738863d6b47ad63786529c9645
Riccco <32475423+Riccco@users.noreply.github.com>
2021-12-29 16:33:39 +0800

    添加getMessage方法 (#290)
    添加getMessage方法 (#290)

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -2,7 +2,12 @@ package com.github.hcsp;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(getMessage());
+        System.out.println( getMessage() );
+    }
+
+    public static String getMessage(){
+        String msg = "Hello";
+       return  msg;
     }
 
     // Create a getMessage() method here to return string "Hello"
