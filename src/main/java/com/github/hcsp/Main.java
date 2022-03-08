57ed2aae1abcea9c9901557d31fbcfe7930ec816
Y.L <100324074+xiangqiangkan@users.noreply.github.com>
2022-03-08 17:11:36 +0800

    创建方法输出hello (#297)
    创建方法输出hello (#297)

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -2,10 +2,15 @@ package com.github.hcsp;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(getMessage());
+        getMessage();
     }
 
     // Create a getMessage() method here to return string "Hello"
     // 在这里编写一个getMessage()方法，返回字符串"Hello"
+     public static boolean getMessage(){
 
+         System.out.println("hello");
+
+         return false;
+     }
 }
