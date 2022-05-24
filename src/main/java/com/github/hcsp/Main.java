4208d853d859fa228ac192c79953b42c76448fe0
Haohao98 <53430416+Haohao98@users.noreply.github.com>
2022-05-25 00:33:59 +0800

    hello (#305)
    hello (#305)

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -1,5 +1,7 @@
 package com.github.hcsp;
 
+import com.sun.org.apache.xpath.internal.objects.XString;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println(getMessage());
@@ -7,5 +9,10 @@ public class Main {
 
     // Create a getMessage() method here to return string "Hello"
     // 在这里编写一个getMessage()方法，返回字符串"Hello"
+    public  static String getMessage()
+    {
+        return "Hello";
+
+    }
 
 }
