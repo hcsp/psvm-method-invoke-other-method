44596c7b383567fc1b609d6e3f17005c4f524761
GJC <599953528@qq.com>
2024-09-05 16:20:59 +0800

    创建静态方法getMessage()

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
     // Create a getMessage() method here to return string "Hello"
     // 在这里编写一个getMessage()方法，返回字符串"Hello"
-    // 由于getMessage()方法是在main方法种调用的，而main方法本身就是静态的。所以创建getMessage方法时也需要是静态的。
+    // 由于 getMessage()方法是在main方法种调用的，而main方法本身就是静态的。所以创建getMessage方法时也需要是静态的。
     public static String getMessage(){
         return "Hello";
     }
